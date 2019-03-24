c62381922b91418472f349e5f59aa61135f8613d
Marko Baricevic <marbar3778@yahoo.com>
2019-03-24 19:48:43 +0100

    made changes to pr

--- client/components/displayticket.jsx
+++ client/components/displayticket.jsx
@@ -1,16 +1,14 @@
-
-import React from 'react';
-import BackIcon from '@material-ui/icons/ArrowBack';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import styled, { createGlobalStyle } from 'styled-components';
-import Link from 'next/link';
-import PropTypes from 'prop-types';
+import React from "react";
+import BackIcon from "@material-ui/icons/ArrowBack";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import styled, { createGlobalStyle } from "styled-components";
+import Link from "next/link";
+import PropTypes from "prop-types";
 // import Background from '../components/Background.jsx';
 
-import QRCode from 'qrcode.react';
-import { tickets } from './Data';
-
+import QRCode from "qrcode.react";
+import { tickets } from "../data/Data";
 
 const GlobalStyles = createGlobalStyle`
     body {
@@ -21,49 +19,48 @@ const GlobalStyles = createGlobalStyle`
     `;
 
 const StyledCell = styled.div`
-    font-size: 2em;
-    color: beige;
-    width: 7em;
-    height: 5em;
-    background: #3b3bff;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    height: 3.1em;
-    padding-left: 1em;
-    padding-right: 1em;
+  font-size: 2em;
+  color: beige;
+  width: 7em;
+  height: 5em;
+  background: #3b3bff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 3.1em;
+  padding-left: 1em;
+  padding-right: 1em;
 `;
 
 const GridContainer = styled.div`
-    margin: auto;
+  margin: auto;
 `;
 
 const StyledHeader = styled.header`
-    justify-content: flex-start; 
-    margin: auto;
-    display: flex;
-    height: 12em;
-    font-family: "arial";
+  justify-content: flex-start;
+  margin: auto;
+  display: flex;
+  height: 12em;
+  font-family: "arial";
 `;
 
 const HeaderButton = styled(Button)`
-    && {
+  && {
     color: white;
     border-color: transparent;
     width: 40%;
     background-color: transparent;
-    }
+  }
 `;
 
 const HeaderIcon = styled(BackIcon)`
-    && {
+  && {
     color: white;
-    margin-right: 25%; 
+    margin-right: 25%;
     height: 100%;
     width: 50%;
-    }
+  }
 `;
-
 
 // TODO: factor divs out
 const DisplayTicket = ({ id }) => (
@@ -88,30 +85,22 @@ const DisplayTicket = ({ id }) => (
 
     <GridContainer>
       <Paper
-        style={{ margin: '5em auto 0 auto', color: 'black', background: 'transparent' }}
+        style={{
+          margin: "5em auto 0 auto",
+          color: "black",
+          background: "transparent"
+        }}
       >
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <div style={{ textAlign: 'left' }}>
-            <StyledCell>
-                Event Name:
-            </StyledCell>
-            <StyledCell>
-                Date:
-            </StyledCell>
-            <StyledCell>
-                Price:
-            </StyledCell>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div style={{ textAlign: "left" }}>
+            <StyledCell>Event Name:</StyledCell>
+            <StyledCell>Date:</StyledCell>
+            <StyledCell>Price:</StyledCell>
           </div>
-          <div style={{ textAlign: 'right' }}>
-            <StyledCell>
-              {tickets[id].eventName}
-            </StyledCell>
-            <StyledCell>
-              {tickets[id].date}
-            </StyledCell>
-            <StyledCell>
-              {`${tickets[id].price.toFixed(2)} €`}
-            </StyledCell>
+          <div style={{ textAlign: "right" }}>
+            <StyledCell>{tickets[id].eventName}</StyledCell>
+            <StyledCell>{tickets[id].date}</StyledCell>
+            <StyledCell>{`${tickets[id].price.toFixed(2)} €`}</StyledCell>
           </div>
         </div>
       </Paper>
@@ -119,45 +108,36 @@ const DisplayTicket = ({ id }) => (
     {tickets[id].block && (
       <GridContainer>
         <Paper
-          style={{ margin: '5em auto 0 auto', color: 'black', background: 'transparent' }}
+          style={{
+            margin: "5em auto 0 auto",
+            color: "black",
+            background: "transparent"
+          }}
         >
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <div style={{ textAlign: 'left' }}>
-              <StyledCell>
-                Block:
-              </StyledCell>
-              <StyledCell>
-                Row:
-              </StyledCell>
-              <StyledCell>
-                Seat:
-              </StyledCell>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <div style={{ textAlign: "left" }}>
+              <StyledCell>Block:</StyledCell>
+              <StyledCell>Row:</StyledCell>
+              <StyledCell>Seat:</StyledCell>
             </div>
-            <div style={{ textAlign: 'right' }}>
-              <StyledCell>
-                {tickets[id].block}
-              </StyledCell>
-              <StyledCell>
-                {tickets[id].row}
-              </StyledCell>
-              <StyledCell>
-                {tickets[id].seat}
-              </StyledCell>
+            <div style={{ textAlign: "right" }}>
+              <StyledCell>{tickets[id].block}</StyledCell>
+              <StyledCell>{tickets[id].row}</StyledCell>
+              <StyledCell>{tickets[id].seat}</StyledCell>
             </div>
           </div>
         </Paper>
       </GridContainer>
     )}
   </div>
-
 );
 
 DisplayTicket.propTypes = {
-  id: PropTypes.string,
+  id: PropTypes.string
 };
 
 DisplayTicket.defaultProps = {
-  id: PropTypes.objectOf(PropTypes.string),
+  id: PropTypes.objectOf(PropTypes.string)
 };
 
 export default DisplayTicket;
